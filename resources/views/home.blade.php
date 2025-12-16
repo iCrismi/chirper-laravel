afe9c3193701c706bee5096d569864e003d5a8f4
iCrismi <icrismff27@outlook.com>
2025-12-16 03:15:04 -0500

    Added a paragraph in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,14 @@
     <x-slot:title>
         Welcome
     </x-slot:title>
-    
+
     <div class="max-w-2xl mx-auto">
         <div class="card bg-base-100 shadow mt-8">
             <div class="card-body">
                 <div>
                     <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
                     <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it sing (or chirp)!</p>
+                    <p class="mt-2 text-sm text-gray-600">Now live on the internet! 🎉</p>
                 </div>
             </div>
         </div>
